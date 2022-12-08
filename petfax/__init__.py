4c8dbea9db9cc1866f10d9e860b27b8dda4012dd
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-12-08 13:28:03 -0500

    part4 complete with minimal styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -3,14 +3,19 @@ from flask import Flask
 #app Factory
 def create_app():
     app = Flask(__name__)
+    
     #index
     @app.route('/')
-    def hello():
+    def index():
         return 'Hello, PetFax'
 
     # register pet blueprint 
     from . import pet
     app.register_blueprint(pet.bp)
+
+    #register pet fact
+    from . import fact
+    app.register_blueprint(fact.bp)
         
     #return the app
     return app
