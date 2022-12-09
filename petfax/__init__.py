f42d2d0dea0b645a7394c2a327de78d4e09c225c
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-12-08 20:28:06 -0500

    connected to database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,17 @@
 #configur
 from flask import Flask
+from flask_migrate import Migrate
+
 #app Factory
 def create_app():
     app = Flask(__name__)
+    #database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
     
     #index
     @app.route('/')
